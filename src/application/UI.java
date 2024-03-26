fd40d2fbec40b27d24c72d94f5e7bb0430c83e45
davi7709 <davialealves@gmail.com>
2024-03-26 15:22:40 -0300

    Knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,9 +57,9 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		if(!chessMatch.getCheckMate()) {
+		if (!chessMatch.getCheckMate()) {
 			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-			if(chessMatch.getCheckMate()) {
+			if (chessMatch.getCheck()) {
 				System.out.println("CHECK!");
 			}
 		}
@@ -67,8 +67,10 @@ public class UI {
 			System.out.println("CHECKMATE!");
 			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
-	
 	}
+
+
+	
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		for(int x=0; x<pieces.length; x++) {
